5660c14cfc4382778807cceef03d8753e9df6e19
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2008-11-17 13:34:54 +0000

    Add support for literate Haskell files, using cpphs's unlit

--- Language/Haskell/Exts.hs
+++ Language/Haskell/Exts.hs
@@ -12,6 +12,8 @@ import Language.Haskell.Exts.Build
 import Language.Haskell.Exts.Syntax
 import Language.Haskell.Exts.Parser
 import Language.Haskell.Exts.Pretty
+import Data.List
+import Language.Preprocessor.Unlit
 
 parseFile :: FilePath -> IO (ParseResult Module)
 parseFile fp = readFile fp >>= (return . parseFileContentsWithMode (ParseMode fp))
@@ -20,7 +22,10 @@ parseFileContents :: String -> ParseResult Module
 parseFileContents = parseFileContentsWithMode defaultParseMode
 
 parseFileContentsWithMode :: ParseMode -> String -> ParseResult Module
-parseFileContentsWithMode p rawStr = parseModuleWithMode p (unlines $ map f $ lines rawStr)
+parseFileContentsWithMode p rawStr = parseModuleWithMode p (delit $ unlines $ map f $ lines rawStr)
     where
         f ('#':_) = ""
         f x = x
+
+        filename = parseFilename p
+        delit = if ".lhs" `isSuffixOf` filename then unlit filename else id
