2f0ca024c0b38a71f379fa0e15a389ebf353289f
Niklas Broberg <nibro@cs.chalmers.se>
2008-12-13 14:41:03 +0200

    Support for magic hash literals
    darcs-hash:20081213124103-deec5-67703fd70f753eac43b5b478698979ac6cdf91aa

--- test/examples/MagicHash.hs
+++ test/examples/MagicHash.hs
@@ -4,3 +4,9 @@ import GHC.Exts
 
 putByteArray :: Int# -> Int#
 putByteArray s# = s# +# 1#
+
+floatFoo :: Float# -> Float
+floatFoo f# = F# (f# `plusFloat#` 1.2e2#)
+
+charFun :: Char# -> Bool
+charFun c# = c# `gtChar#` 'a'#
