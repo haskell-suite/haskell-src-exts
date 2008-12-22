817175f2eac1885fc850a25f39f33734e016a225
Neil Mitchell <none>
2008-12-22 15:23:00 +0200

    Add expected failures to the test suite
    darcs-hash:20081222132300-73ab9-335687f53a99c20ee8a774d61810be90df7a8023

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Language.Haskell.Exts
 import System.IO
+import Control.Monad
 import Data.List
 import System.Directory
 
@@ -11,16 +12,18 @@ main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
     files <- getDirectoryContents "examples"
-    mapM_ check $ ["examples/" ++ x | x <- files, not $ "." `isPrefixOf` x]
+    src <- liftM lines $ readFile "failing.txt"
+    sequence_ [check (x `elem` src) ("examples/" ++ x) | x <- files, not $ "." `isPrefixOf` x]
     putStrLn "\nAll tests completed"
 
 
-check :: FilePath -> IO ()
-check file = do
+check :: Bool -> FilePath -> IO ()
+check expected file = do
     res <- parseFile file
     case res of
         ParseOk x -> putChar '.'
-        err -> putStrLn $ "\nFailure when parsing " ++ show file ++ "\n" ++ show err
+        err | expected -> putChar '!'
+            | otherwise -> putStrLn $ "\nFailure when parsing " ++ show file ++ "\n" ++ show err
 
 
     
