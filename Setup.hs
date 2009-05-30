7037c9fd1fc7924e31f444f2b14f9463b7930a4f
Niklas Broberg <niklas.broberg@gmail.com>
2009-05-30 15:06:49 +0300

    Uncomment the test machinery from Setup to make it compile
    Ignore-this: f989f97ba8db85a9e924a31b363848fe
    
    darcs-hash:20090530120649-710fa-05e2e7532d3dbe11fea54f2942ddb5c72912bb20

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,3 @@
 import Distribution.Simple
-import Test.Runner ( go )
-main = defaultMainWithHooks $ simpleUserHooks { runTests = \args _ _ _ -> go args }
+--import Test.Runner ( go )
+main = defaultMain --WithHooks $ simpleUserHooks { runTests = \args _ _ _ -> go args }
