7b20179d1c0dab2a60b022c26b1d89a5043a8738
Niklas Broberg <niklas.broberg@gmail.com>
2009-06-22 00:38:18 +0000

    Break the internal ExtScheme stuff out into its own internal module

---
+++ src/Language/Haskell/Exts/ExtScheme.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_HADDOCK hide #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Language.Haskell.Exts.ExtScheme
+-- Copyright   :  (c) Niklas Broberg 2009
+-- License     :  BSD-style (see the file LICENSE.txt)
+--
+-- Maintainer  :  Niklas Broberg, d00nibro@chalmers.se
+-- Stability   :  stable
+-- Portability :  portable
+--
+-- Internal scheme for handling extensions in a
+-- convenient fashion.
+--
+-----------------------------------------------------------------------------
+module Language.Haskell.Exts.ExtScheme where
+
+import Language.Haskell.Exts.Extension
+
+data ExtScheme = Any [Extension] | All [Extension]
+  deriving (Eq,Show)
+
+type MExtScheme = Maybe ExtScheme
+
+class Enabled a where
+  isEnabled :: a -> [Extension] -> Bool
+
+instance Enabled Extension where
+  isEnabled = elem
+
+instance Enabled ExtScheme where
+  isEnabled (Any exts) enabled = any (`elem` enabled) exts
+  isEnabled (All exts) enabled = all (`elem` enabled) exts
+
+instance Enabled a => Enabled (Maybe a) where
+  isEnabled Nothing  = const True
+  isEnabled (Just a) = isEnabled a
