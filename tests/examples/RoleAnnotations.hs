585a80cd44c7788afa1aa282e2595215cc08c3ba
Matthew Pickering <matthewtpickering@gmail.com>
2015-07-27 14:41:46 +0200

    Add RoleAnnotations test

---
+++ tests/examples/RoleAnnotations.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE RoleAnnotations, PolyKinds, KindSignatures #-}
+
+module Roles where
+
+data T1 a = K1 a
+data T2 a = K2 a
+
+data T5 a = K5 a
+data T6 a = K6
+data T7 a b = K7 b
+
+type role T1 nominal
+type role T2 representational
+type role T3 phantom
+type role T4 nominal _
+type role T5   _
