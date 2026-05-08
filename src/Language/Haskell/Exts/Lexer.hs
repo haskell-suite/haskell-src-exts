674ff8cc476de9e2f047c9ba9a4ef59e7eae3a59
Michal J. Gajda <mjgajda@gmail.com>
2026-05-08 14:46:54 +0000

    Expose Text-input lexer entry points
    Adds lexTokenStreamText and lexTokenStreamTextWithMode in
    Language.Haskell.Exts.Lexer, the lexer-only counterparts of the
    Text-input parser entry points in Language.Haskell.Exts.Parser.Text.
    
    These let consumers that only need a token stream (linters, syntax
    highlighters, exact-print tooling) skip the eager Data.Text.pack at
    the String boundary that the existing String entry point performs.
    
    Also factors lexIt out as a top-level helper shared by both the
    String and Text variants.

--- src/Language/Haskell/Exts/Lexer.hs
+++ src/Language/Haskell/Exts/Lexer.hs
@@ -16,6 +16,7 @@
 module Language.Haskell.Exts.Lexer
     (
       lexTokenStream, lexTokenStreamWithMode,
+      lexTokenStreamText, lexTokenStreamTextWithMode,
 
       Token(..), Loc(..),
 
@@ -26,6 +27,7 @@ module Language.Haskell.Exts.Lexer
 import Language.Haskell.Exts.InternalLexer
 import Language.Haskell.Exts.ParseMonad
 import Language.Haskell.Exts.SrcLoc
+import Data.Text (Text)
 
 -- | Lex a string into a list of Haskell 2010 source tokens.
 lexTokenStream :: String -> ParseResult [Loc Token]
@@ -34,12 +36,21 @@ lexTokenStream = lexTokenStreamWithMode defaultParseMode
 -- | Lex a string into a list of Haskell source tokens, using an explicit mode.
 lexTokenStreamWithMode :: ParseMode -> String -> ParseResult [Loc Token]
 lexTokenStreamWithMode mode = runParserWithMode mode lexIt
-  where lexIt :: P [Loc Token]
-        lexIt = runL go return
-        go :: Lex [Loc Token] [Loc Token]
-        go = do
-          ltok <- topLexer
-          case ltok of
-            Loc _ EOF -> return []
-            _ -> do ts <- go
-                    return (ltok:ts)
+
+-- | 'Text'-input variant of 'lexTokenStream'.  Skips the eager 'T.pack'
+-- that the 'String' entry point performs at the boundary.
+lexTokenStreamText :: Text -> ParseResult [Loc Token]
+lexTokenStreamText = lexTokenStreamTextWithMode defaultParseMode
+
+-- | 'Text'-input variant of 'lexTokenStreamWithMode'.
+lexTokenStreamTextWithMode :: ParseMode -> Text -> ParseResult [Loc Token]
+lexTokenStreamTextWithMode mode = fmap fst . runParserWithModeCommentsText mode lexIt
+
+lexIt :: P [Loc Token]
+lexIt = runL go return
+  where go :: Lex [Loc Token] [Loc Token]
+        go = do ltok <- topLexer
+                case ltok of
+                  Loc _ EOF -> return []
+                  _         -> do ts <- go
+                                  return (ltok:ts)
