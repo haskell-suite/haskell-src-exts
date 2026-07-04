d7d774efd0d30c4d649121f5ad2a9f9b694ad749
Dan Burton <danburton.email@gmail.com>
2026-07-03 20:38:53 -0700

    test: less exhaustive

--- tests/Extensions.hs
+++ tests/Extensions.hs
@@ -22,12 +22,30 @@ infix 3 ~~
 (~~) :: Monad m => [Extension] -> [Extension] -> Property m
 xts1 ~~ xts2 = forAll $ \lang -> ((==) `on` sort . toExtensionList lang) xts1 xts2
 
+-- arbitrary examples
+e1 :: KnownExtension
+e1 = OverlappingInstances
+
+e2 :: KnownExtension
+e2 = UndecidableInstances
+
 extensionProperties :: TestTree
 extensionProperties =
   localOption (SmallCheckDepth 2) $ testGroup "Properties of LANGUAGE extensions" $
   [ testProperty "identity" $ \x -> x ~~ x
   , testProperty "idempotence" $ \x -> x ++ x ~~ x
-  , testProperty "right bias" $ \x y -> x ++ y ++ x ~~ y ++ x
+  -- Actually, let's not exhaustively check n^2 cases
+  -- , testProperty "right bias" $ \x y -> x ++ y ++ x ~~ y ++ x
+  , testProperty "right bias - override to disabled" $
+      [EnableExtension e1, DisableExtension e1] ~~ [DisableExtension e1]
+  , testProperty "right bias - override to enabled" $
+      [DisableExtension e1, EnableExtension e1] ~~ [EnableExtension e1]
+  , testProperty "right bias - interleaved override to disabled" $
+      [EnableExtension e1, EnableExtension e2, DisableExtension e1]
+        ~~ [EnableExtension e2, DisableExtension e1]
+  , testProperty "right bias - interleaved override to enabled" $
+      [DisableExtension e1, EnableExtension e2, EnableExtension e1]
+        ~~ [EnableExtension e2, EnableExtension e1]
   , testProperty "closedness of implication" $ \x -> impliesExts (impliesExts x) == impliesExts x
   , testProperty "closedness of toExtensionList" $ \l x -> let es = toExtensionList l x in es == impliesExts es
   , testProperty "opposite extensions 1" $ \x -> [EnableExtension x, DisableExtension x] ~~ [DisableExtension x]
